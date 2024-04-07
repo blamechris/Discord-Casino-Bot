0c235901b116e9b09fada26a2a76bbaaf7bd7b0c
Christopher <christopher.pishaki.533@my.csun.edu>
2024-04-06 19:31:52 -0700

    Updates to add bot functionality for basic black jack

--- discordbot/botmain.py
+++ discordbot/botmain.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import mysql.connector
 import os
+import random
 
 # Enable default intents
 intents = discord.Intents.default()
@@ -34,6 +35,27 @@ async def mystats(ctx):
         await ctx.send(f"Your stats: Chips: {user_data['chip_count']}")
     else:
         await ctx.send("You don't have an account. Please create one using !createaccount.")
+
+suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+deck = [(value, suit) for suit in suits for value in values]
+
+def draw_card():
+    card = random.choice(deck)  # Draw a random card from the deck
+    deck.remove(card)  # Remove the drawn card from the deck
+    return card
+
+def shuffle_deck():
+    random.shuffle(deck)  # Shuffle the deck
+
+def eval_hand(hand_values):
+    if sum(hand_values) > 21:
+        return 'BUST'
+    elif sum(hand_values) == 21:
+        return 'BLACKJACK'
+    else:
+        return 'You have: {sum(hand_values)}, do you want to hit or stand?'
 
 
 TOKEN = os.environ.get('BOT_TOKEN')
