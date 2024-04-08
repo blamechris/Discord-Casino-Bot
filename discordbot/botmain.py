e33430b29e7ad44f35bac32e964daea1023cd218
Christopher <christopher.pishaki.533@my.csun.edu>
2024-04-07 17:54:53 -0700

    Main blackjack function working. Also edit to get rid of some warnings from the coderunner file

--- discordbot/botmain.py
+++ discordbot/botmain.py
@@ -5,6 +5,7 @@ import os
 import random
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()  # This loads the environment variables from .env
 
@@ -41,6 +42,47 @@ async def mystats(ctx):
     #     await ctx.send("You don't have an account. Please create one using !createaccount.")
     print("mystats")
 
+@bot.command()
+async def blackjack(ctx):
+    shuffle_deck()  # Make sure the deck is shuffled before each game
+    
+    player_hand = [draw_card(), draw_card()]
+    dealer_hand = [draw_card(), draw_card()]
+
+    await ctx.send(f"Your hand: {player_hand[0]}, {player_hand[1]}. Do you want to 'hit' or 'stand'?")
+    
+    def check(m):
+        return m.author == ctx.author and m.content.lower() in ["hit", "stand"]
+    
+    try:
+        msg = await bot.wait_for('message', check=check, timeout=30.0)  # Wait for player's response
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to respond. Game over.")
+        return
+
+    while msg.content.lower() == "hit":
+        player_hand.append(draw_card())
+        player_value = calculate_hand_value(player_hand)
+        if player_value > 21:
+            await ctx.send(f"Card drawn: {player_hand[-1]}. Total: {player_value}. You busted!")
+            return
+        elif player_value == 21:
+            await ctx.send(f"Card drawn: {player_hand[-1]}. Total: {player_value}. Blackjack!")
+            break
+        else:
+            await ctx.send(f"Card drawn: {player_hand[-1]}. Total: {player_value}. Do you want to 'hit' or 'stand'?")
+            try:
+                msg = await bot.wait_for('message', check=check, timeout=30.0)
+            except asyncio.TimeoutError:
+                await ctx.send("Sorry, you took too long to respond. Game over.")
+                return
+
+    # Dealer's turn to draw cards
+    while calculate_hand_value(dealer_hand) < 17:
+        dealer_hand.append(draw_card())
+
+    await end_game(ctx, player_hand, dealer_hand)
+
 suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
 values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
@@ -62,6 +104,35 @@ def eval_hand(hand_values):
     else:
         return 'You have: {sum(hand_values)}, do you want to hit or stand?'
 
+def calculate_hand_value(hand):
+    """Calculate the value of a hand, taking Aces into account."""
+    value = 0
+    aces = 0
+    for card in hand:
+        if card[0] in ['Jack', 'Queen', 'King']:
+            value += 10
+        elif card[0] == 'Ace':
+            aces += 1
+            value += 11  # Initially count aces as 11
+        else:
+            value += int(card[0])
+    
+    # Adjust for aces if value goes over 21
+    while value > 21 and aces:
+        value -= 10  # Convert an ace from 11 to 1
+        aces -= 1
+
+    return value
+
+async def end_game(ctx, player_hand, dealer_hand):
+    player_value = calculate_hand_value(player_hand)
+    dealer_value = calculate_hand_value(dealer_hand)
+    if player_value > 21:
+        await ctx.send(f"You busted with a total of {player_value}. Dealer wins.")
+    elif dealer_value > 21 or player_value > dealer_value:
+        await ctx.send(f"You win! You had {player_value} and the dealer had {dealer_value}.")
+    else:
+        await ctx.send(f"Dealer wins with {dealer_value} against your {player_value}.")
 
 TOKEN = os.environ.get('BOT_TOKEN')
 
